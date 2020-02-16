73ee4af73309c4d20cf7b28ac82a5e1037a5d65a
Sébastien Eustace <sebastien@eustace.io>
2020-02-16 14:58:30 +0100

    Add the to_dependency method to ProjectPackage

--- poetry_core/packages/project_package.py
+++ poetry_core/packages/project_package.py
@@ -22,6 +22,13 @@ class ProjectPackage(Package):
     def is_root(self):
         return True
 
+    def to_dependency(self):
+        dependency = super(ProjectPackage, self).to_dependency()
+
+        dependency.is_root = True
+
+        return dependency
+
     @property
     def python_versions(self):
         return self._python_versions
