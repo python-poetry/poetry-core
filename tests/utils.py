60b3071bcedc8758e6522d4324c9fbc70be979f9
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2020-04-18 01:32:46 +0200

    tests: add minimal pep517 integration checks

---
+++ tests/utils.py
@@ -0,0 +1,52 @@
+import shutil
+
+from contextlib import contextmanager
+from typing import Any
+from typing import ContextManager
+from typing import Dict
+from typing import Optional
+
+from poetry.core.utils._compat import Path
+from poetry.core.utils.toml_file import TomlFile
+
+
+try:
+    from backports import tempfile
+except ImportError:
+    import tempfile
+
+
+__toml_build_backend_patch__ = {
+    "build-system": {
+        "requires": [str(Path(__file__).parent.parent)],
+        "build-backend": "poetry.core.masonry.api",
+    }
+}
+
+
+@contextmanager
+def temporary_project_directory(
+    path, toml_patch=None
+):  # type: (Path, Optional[Dict[str, Any]]) -> ContextManager[str]
+    """
+    Context manager that takes a project source directory, copies content to a temporary
+    directory, patches the `pyproject.toml` using the provided patch, or using the default
+    patch if one is not given. The default path replaces `build-system` section in order
+    to use the working copy of poetry-core as the backend.
+
+    Once the context, exists, the temporary directory is cleaned up.
+
+    :param path: Source project root directory to copy from.
+    :param toml_patch: Patch to use for the pyproject.toml, defaults to build system patching.
+    :return: A temporary copy
+    """
+    assert (path / "pyproject.toml").exists()
+
+    with tempfile.TemporaryDirectory(prefix="poetry-core-pep517") as tmp:
+        dst = Path(tmp) / path.name
+        shutil.copytree(str(path), dst)
+        toml = TomlFile(str(dst / "pyproject.toml"))
+        data = toml.read()
+        data.update(toml_patch or __toml_build_backend_patch__)
+        toml.write(data)
+        yield str(dst)
