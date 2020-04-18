5fee959c04cc0960e88a882a80b5c9cbebafb39e
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2020-04-18 15:07:29 +0200

    tests: parametrize pep517 integration cases

--- tests/integration/test_pep517.py
+++ tests/integration/test_pep517.py
@@ -7,16 +7,14 @@ from tests.utils import temporary_project_directory
 pytestmark = pytest.mark.integration
 
 
-def test_pep517_simple_project(common_project):
-    with temporary_project_directory(common_project("simple_project")) as path:
-        assert check(path)
-
-
-def test_pep517_src_extended(masonry_project):
-    with temporary_project_directory(masonry_project("src_extended")) as path:
-        assert check(path)
-
-
-def test_pep517_disable_setup_py(masonry_project):
-    with temporary_project_directory(masonry_project("disable_setup_py")) as path:
+@pytest.mark.parametrize(
+    "getter, project",
+    [
+        ("common_project", "simple_project"),
+        ("masonry_project", "src_extended"),
+        ("masonry_project", "disable_setup_py"),
+    ],
+)
+def test_pep517_check(request, getter, project):
+    with temporary_project_directory(request.getfixturevalue(getter)(project)) as path:
         assert check(path)
