84f576806d40ee91e7a186f7e35e39f701a6ab27
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2020-08-10 12:52:38 +0200

    Support specifying build time executable for wheels

--- poetry/core/masonry/builder.py
+++ poetry/core/masonry/builder.py
@@ -1,3 +1,8 @@
+from typing import Optional
+from typing import Union
+
+from poetry.core.utils._compat import Path
+
 from .builders.sdist import SdistBuilder
 from .builders.wheel import WheelBuilder
 
@@ -11,7 +16,9 @@ class Builder:
     def __init__(self, poetry):
         self._poetry = poetry
 
-    def build(self, fmt="all"):
+    def build(
+        self, fmt, executable=None
+    ):  # type: (str, Optional[Union[str, Path]]) -> None
         if fmt in self._FORMATS:
             builders = [self._FORMATS[fmt]]
         elif fmt == "all":
@@ -20,4 +27,4 @@ class Builder:
             raise ValueError("Invalid format: {}".format(fmt))
 
         for builder in builders:
-            builder(self._poetry).build()
+            builder(self._poetry, executable=executable).build()
