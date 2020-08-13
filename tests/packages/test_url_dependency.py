b82168cb71aae2f1e3ea61fd4864322b9162a8e3
finswimmer <finswimmer77@gmail.com>
2020-08-13 07:24:31 +0200

    add whitespace before separator between url and markers for PEP508 url dependency string

---
+++ tests/packages/test_url_dependency.py
@@ -0,0 +1,23 @@
+from poetry.core.packages import URLDependency
+from poetry.core.version.markers import SingleMarker
+
+
+def test_to_pep_508():
+    dependency = URLDependency(
+        "pytorch",
+        "https://download.pytorch.org/whl/cpu/torch-1.5.1%2Bcpu-cp38-cp38-linux_x86_64.whl",
+    )
+
+    expected = "pytorch @ https://download.pytorch.org/whl/cpu/torch-1.5.1%2Bcpu-cp38-cp38-linux_x86_64.whl"
+    assert expected == dependency.to_pep_508()
+
+
+def test_to_pep_508_with_marker():
+    dependency = URLDependency(
+        "pytorch",
+        "https://download.pytorch.org/whl/cpu/torch-1.5.1%2Bcpu-cp38-cp38-linux_x86_64.whl",
+    )
+    dependency.marker = SingleMarker("sys.platform", "linux")
+
+    expected = 'pytorch @ https://download.pytorch.org/whl/cpu/torch-1.5.1%2Bcpu-cp38-cp38-linux_x86_64.whl ; sys_platform == "linux"'
+    assert expected == dependency.to_pep_508()
