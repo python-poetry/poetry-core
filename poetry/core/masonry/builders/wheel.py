6e77e3b343b6221c541e964a1fbe8aa26448b732
Jonathan Frere <j.frere@hzdr.de>
2020-08-17 15:30:11 +0200

    Add pathsep for Windows path matching

--- poetry/core/masonry/builders/wheel.py
+++ poetry/core/masonry/builders/wheel.py
@@ -173,7 +173,7 @@ class WheelBuilder(Builder):
             license_files_to_add.append(self._path / base)
             license_files_to_add.extend(self._path.glob(base + ".*"))
 
-        license_files_to_add.extend(self._path.glob("LICENSES/**/*"))
+        license_files_to_add.extend((self._path / "LICENSES").glob("**" + os.pathsep + "*"))
 
         for path in license_files_to_add:
             if path.is_file():
