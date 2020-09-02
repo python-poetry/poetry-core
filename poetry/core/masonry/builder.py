a467e1a8ec682e459cfe7cd97f8d7cebb53f3ce3
finswimmer <finswimmer77@gmail.com>
2020-09-02 20:53:58 +0200

    execute SdistBuilder and WheelBuilder separately

--- poetry/core/masonry/builder.py
+++ poetry/core/masonry/builder.py
@@ -1,11 +1,13 @@
-from .builders.complete import CompleteBuilder
 from .builders.sdist import SdistBuilder
 from .builders.wheel import WheelBuilder
 
 
 class Builder:
-
-    _FORMATS = {"sdist": SdistBuilder, "wheel": WheelBuilder, "all": CompleteBuilder}
+    _FORMATS = {
+        "sdist": [SdistBuilder],
+        "wheel": [WheelBuilder],
+        "all": [SdistBuilder, WheelBuilder],
+    }
 
     def __init__(self, poetry):
         self._poetry = poetry
@@ -14,6 +16,5 @@ class Builder:
         if fmt not in self._FORMATS:
             raise ValueError("Invalid format: {}".format(fmt))
 
-        builder = self._FORMATS[fmt](self._poetry)
-
-        return builder.build()
+        for builder in self._FORMATS[fmt]:
+            builder(self._poetry).build()
