b0fd58d28fec44e672e525a931156edee7ba9632
finswimmer <finswimmer77@gmail.com>
2020-09-04 21:25:14 +0200

    remove "all" from _FORMATS and read all values instead if key word appears

--- poetry/core/masonry/builder.py
+++ poetry/core/masonry/builder.py
@@ -4,17 +4,20 @@ from .builders.wheel import WheelBuilder
 
 class Builder:
     _FORMATS = {
-        "sdist": [SdistBuilder],
-        "wheel": [WheelBuilder],
-        "all": [SdistBuilder, WheelBuilder],
+        "sdist": SdistBuilder,
+        "wheel": WheelBuilder,
     }
 
     def __init__(self, poetry):
         self._poetry = poetry
 
-    def build(self, fmt):
-        if fmt not in self._FORMATS:
+    def build(self, fmt="all"):
+        if fmt in self._FORMATS:
+            builders = [self._FORMATS[fmt]]
+        elif fmt == "all":
+            builders = self._FORMATS.values()
+        else:
             raise ValueError("Invalid format: {}".format(fmt))
 
-        for builder in self._FORMATS[fmt]:
+        for builder in builders:
             builder(self._poetry).build()
