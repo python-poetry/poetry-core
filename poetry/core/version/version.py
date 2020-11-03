57f3de65979beeea2cbbf1c2e0ad579f46b35b0f
Yajing Zhao <32400676+Yajing-Zhao@users.noreply.github.com>
2020-11-02 19:26:45 -0700

    Update VERSION_PATTERN
    The VERSION_PATTERN should be compatible with semantic version guideline. I replace the old pattern string with the official recommendation in this fix.

--- poetry/core/version/version.py
+++ poetry/core/version/version.py
@@ -12,41 +12,9 @@ _Version = namedtuple("_Version", ["epoch", "release", "dev", "pre", "post", "lo
 
 
 VERSION_PATTERN = re.compile(
-    r"""
-    ^
-    v?
-    (?:
-        (?:(?P<epoch>[0-9]+)!)?                           # epoch
-        (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
-        (?P<pre>                                          # pre-release
-            [-_.]?
-            (?P<pre_l>(a|b|c|rc|alpha|beta|pre|preview))
-            [-_.]?
-            (?P<pre_n>[0-9]+)?
-        )?
-        (?P<post>                                         # post release
-            (?:-(?P<post_n1>[0-9]+))
-            |
-            (?:
-                [-_.]?
-                (?P<post_l>post|rev|r)
-                [-_.]?
-                (?P<post_n2>[0-9]+)?
-            )
-        )?
-        (?P<dev>                                          # dev release
-            [-_.]?
-            (?P<dev_l>dev)
-            [-_.]?
-            (?P<dev_n>[0-9]+)?
-        )?
-    )
-    (?:\+(?P<local>[a-z0-9]+(?:[-_.][a-z0-9]+)*))?       # local version
-    $
-""",
+    "^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$",
     re.IGNORECASE | re.VERBOSE,
 )
-
 
 class Version(BaseVersion):
     def __init__(self, version):
