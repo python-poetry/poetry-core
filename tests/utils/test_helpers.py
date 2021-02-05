1034515a9d441c8dcab63bedfc30d5ad68d5ce7d
Sébastien Eustace <sebastien@eustace.io>
2021-02-05 16:45:01 +0100

    Revert changes to canonicalize_name (#132)

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -62,9 +62,7 @@ isort@ git+git://github.com/timothycrosley/isort.git@e63ae06ec7d70b06df9e5283576
     assert result == expected
 
 
-@pytest.mark.parametrize(
-    "raw", ["a-b-c", "a.b-c", "a.b.c", "a_b-c", "a_b_c", "a-b_c", "a.b_c", "a-b.c"]
-)
+@pytest.mark.parametrize("raw", ["a-b-c", "a_b-c", "a_b_c", "a-b_c"])
 def test_utils_helpers_canonical_names(raw):
     assert canonicalize_name(raw) == "a-b-c"
 
