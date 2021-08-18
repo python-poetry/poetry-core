00ad793e97a51dd3449758032790dc048aae53ae
Andrew Standley <andrew.standley@plangrid.com>
2021-08-18 15:25:58 -0700

    Added allows method to BaseConstraint as it appears to be part of the interface.

--- poetry/core/packages/constraints/base_constraint.py
+++ poetry/core/packages/constraints/base_constraint.py
@@ -6,6 +6,9 @@ if TYPE_CHECKING:
 
 
 class BaseConstraint(object):
+    def allows(self, other: "ConstraintTypes") -> bool:
+        raise NotImplementedError()
+
     def allows_all(self, other: "ConstraintTypes") -> bool:
         raise NotImplementedError()
 
