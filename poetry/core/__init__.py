e68b69d5c1a4e2f21665aea0a49fe994add14993
Sébastien Eustace <sebastien@eustace.io>
2021-08-27 10:26:27 +0200

    Bump version to 1.0.5

--- poetry/core/__init__.py
+++ poetry/core/__init__.py
@@ -7,7 +7,7 @@ except ImportError:
     # noinspection PyUnresolvedReferences
     from pathlib2 import Path
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 __vendor_site__ = (Path(__file__).parent / "_vendor").as_posix()
 
