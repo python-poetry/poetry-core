144cc51b88af12d20cbac9c92cc4ac59b8403159
Daniel Eades <danieleades@hotmail.com>
2021-11-13 12:44:05 +0000

    address mypy errors in core.semver.empty_constraint

--- poetry/core/semver/empty_constraint.py
+++ poetry/core/semver/empty_constraint.py
@@ -1,11 +1,4 @@
-from typing import TYPE_CHECKING
-
 from .version_constraint import VersionConstraint
-
-
-if TYPE_CHECKING:
-    from . import VersionTypes  # noqa
-    from .version import Version  # noqa
 
 
 class EmptyConstraint(VersionConstraint):
@@ -15,22 +8,22 @@ class EmptyConstraint(VersionConstraint):
     def is_any(self) -> bool:
         return False
 
-    def allows(self, version: "Version") -> bool:
+    def allows(self, _version: VersionConstraint) -> bool:
         return False
 
-    def allows_all(self, other: "VersionTypes") -> bool:
+    def allows_all(self, other: VersionConstraint) -> bool:
         return other.is_empty()
 
-    def allows_any(self, other: "VersionTypes") -> bool:
+    def allows_any(self, _other: VersionConstraint) -> bool:
         return False
 
-    def intersect(self, other: "VersionTypes") -> "EmptyConstraint":
+    def intersect(self, _other: VersionConstraint) -> "EmptyConstraint":
         return self
 
-    def union(self, other: "VersionTypes") -> "VersionTypes":
+    def union(self, other: VersionConstraint) -> VersionConstraint:
         return other
 
-    def difference(self, other: "VersionTypes") -> "EmptyConstraint":
+    def difference(self, _other: VersionConstraint) -> "EmptyConstraint":
         return self
 
     def __str__(self) -> str:
