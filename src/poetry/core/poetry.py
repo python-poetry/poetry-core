3911598f0322ac8bfd8b40becb91fd01b0d9135a
davidvujic <david@vujic.se>
2022-03-26 08:39:43 +0100

    fix: add missing pathlib.Path requirement

--- src/poetry/core/poetry.py
+++ src/poetry/core/poetry.py
@@ -20,6 +20,8 @@ class Poetry:
         local_config: dict[str, Any],
         package: ProjectPackage,
     ) -> None:
+        from pathlib import Path
+
         from poetry.core.pyproject.toml import PyProjectTOML
         from poetry.core.utils import workspaces
 
