c5c5138912aef495cab61d8977bc4d9503266490
David Hotham <david.hotham@blueyonder.co.uk>
2022-05-23 23:53:44 +0100

    use importlib.metadata to fetch version

--- src/poetry/core/__init__.py
+++ src/poetry/core/__init__.py
@@ -1,11 +1,23 @@
 from __future__ import annotations
 
+import contextlib
 import sys
 
 from pathlib import Path
 
 
-__version__ = "1.1.0a7"
+__version__ = "0.0.0+in-tree"
+
+with contextlib.suppress(ImportError):
+    if sys.version_info < (3, 8):
+        # compatibility for python <3.8
+        import importlib_metadata as metadata
+    else:
+        from importlib import metadata
+
+    with contextlib.suppress(metadata.PackageNotFoundError):
+        __version__ = metadata.version("poetry-core")  # type: ignore[no-untyped-call]
+
 
 __vendor_site__ = (Path(__file__).parent / "_vendor").as_posix()
 
