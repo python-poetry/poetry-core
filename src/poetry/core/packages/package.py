285bca6c247f6cd4ab1904b4784d41fce257bcae
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-07-05 13:37:16 -0400

    Allow : in Author name.
    See: https://www.fortheclaimofthelife.com/

--- src/poetry/core/packages/package.py
+++ src/poetry/core/packages/package.py
@@ -30,7 +30,7 @@ if TYPE_CHECKING:
 
     T = TypeVar("T", bound="Package")
 
-AUTHOR_REGEX = re.compile(r"(?u)^(?P<name>[- .,\w\d'’\"()&]+)(?: <(?P<email>.+?)>)?$")
+AUTHOR_REGEX = re.compile(r"(?u)^(?P<name>[- .,\w\d'’\"():&]+)(?: <(?P<email>.+?)>)?$")
 
 
 class Package(PackageSpecification):
