52c4d1d36b2d3f18a34d14b101c4c02ad976de9d
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-10-11 23:50:32 +0200

    build: update vendored dependencies

--- src/poetry/core/_vendor/lark/tree.py
+++ src/poetry/core/_vendor/lark/tree.py
@@ -5,6 +5,7 @@ from typing import List, Callable, Iterator, Union, Optional, Generic, TypeVar, 
 
 if TYPE_CHECKING:
     from .lexer import TerminalDef, Token
+    import rich
     if sys.version_info >= (3, 8):
         from typing import Literal
     else:
@@ -142,6 +143,20 @@ class Tree(Generic[_Leaf_T]):
         del queue
         return reversed(list(subtrees.values()))
 
+    def iter_subtrees_topdown(self):
+        """Breadth-first iteration.
+
+        Iterates over all the subtrees, return nodes in order like pretty() does.
+        """
+        stack = [self]
+        while stack:
+            node = stack.pop()
+            if not isinstance(node, Tree):
+                continue
+            yield node
+            for child in reversed(node.children):
+                stack.append(child)
+
     def find_pred(self, pred: 'Callable[[Tree[_Leaf_T]], bool]') -> 'Iterator[Tree[_Leaf_T]]':
         """Returns all nodes of the tree that evaluate pred(node) as true."""
         return filter(pred, self.iter_subtrees())
@@ -178,20 +193,6 @@ class Tree(Generic[_Leaf_T]):
             else:
                 if pred(c):
                     yield c
-
-    def iter_subtrees_topdown(self):
-        """Breadth-first iteration.
-
-        Iterates over all the subtrees, return nodes in order like pretty() does.
-        """
-        stack = [self]
-        while stack:
-            node = stack.pop()
-            if not isinstance(node, Tree):
-                continue
-            yield node
-            for child in reversed(node.children):
-                stack.append(child)
 
     def __deepcopy__(self, memo):
         return type(self)(self.data, deepcopy(self.children, memo), meta=self._meta)
