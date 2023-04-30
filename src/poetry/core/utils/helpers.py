f314393bb9780f2381db7794c98e678344fad7ec
eblis <eblis102@gmail.com>
2023-04-30 14:57:25 +0300

    Fix problem with deleting temporary folders on Windows (#460)

--- src/poetry/core/utils/helpers.py
+++ src/poetry/core/utils/helpers.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 import os
 import shutil
 import stat
+import sys
 import tempfile
+import time
 import unicodedata
 import warnings
 
@@ -40,9 +42,16 @@ def normalize_version(version: str) -> str:
 
 @contextmanager
 def temporary_directory(*args: Any, **kwargs: Any) -> Iterator[str]:
-    name = tempfile.mkdtemp(*args, **kwargs)
-    yield name
-    safe_rmtree(name)
+    if sys.version_info >= (3, 10):
+        # mypy reports an error if ignore_cleanup_errors is
+        # specified literally in the call
+        kwargs["ignore_cleanup_errors"] = True
+        with tempfile.TemporaryDirectory(*args, **kwargs) as name:
+            yield name
+    else:
+        name = tempfile.mkdtemp(*args, **kwargs)
+        yield name
+        robust_rmtree(name)
 
 
 def parse_requires(requires: str) -> list[str]:
@@ -90,10 +99,30 @@ def _on_rm_error(func: Any, path: str | Path, exc_info: Any) -> None:
     func(path)
 
 
-def safe_rmtree(path: str | Path) -> None:
-    if Path(path).is_symlink():
-        return os.unlink(str(path))
+def robust_rmtree(path: str | Path, max_timeout: float = 1) -> None:
+    """
+    Robustly tries to delete paths.
+    Retries several times if an OSError occurs.
+    If the final attempt fails, the Exception is propagated
+    to the caller.
+    """
+    path = Path(path)  # make sure this is a Path object, not str
+    timeout = 0.001
+    while timeout < max_timeout:
+        try:
+            # both os.unlink and shutil.rmtree can throw exceptions on Windows
+            # if the files are in use when called
+            if path.is_symlink():
+                path.unlink()
+            else:
+                shutil.rmtree(path)
+            return  # Only hits this on success
+        except OSError:
+            # Increase the timeout and try again
+            time.sleep(timeout)
+            timeout *= 2
 
+    # Final attempt, pass any Exceptions up to caller.
     shutil.rmtree(path, onerror=_on_rm_error)
 
 
