0cc040f60ef79092de96fe9537b6722ac96faebd
Maarten Derickx <maarten.derickx@navara.nl>
2023-06-12 21:49:32 +0200

    Address second round of comments

--- tests/packages/test_dependency.py
+++ tests/packages/test_dependency.py
@@ -137,6 +137,7 @@ def test_to_pep_508_with_patch_python_version(
     expected = f"Django (>=1.23,<2.0) ; {marker}"
 
     assert dependency.to_pep_508() == expected
+    assert dependency.to_pep_508(resolved=True) == expected
     assert str(dependency.marker) == marker
 
 
