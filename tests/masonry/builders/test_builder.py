efa0a5a061ce1b99ff6157c0ff373e289b5437a5
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2024-08-16 13:56:04 +0200

    Add Python 3.13 to `PYTHON_VERSION` and `AVAILABLE_PYTHONS` (#747)

--- tests/masonry/builders/test_builder.py
+++ tests/masonry/builders/test_builder.py
@@ -114,6 +114,7 @@ def test_get_metadata_content() -> None:
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Software Development :: Build Tools",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ]
